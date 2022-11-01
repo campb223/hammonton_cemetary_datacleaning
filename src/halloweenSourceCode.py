b08439da89051d75796cfdf10976fe154936eb4f
Dale <campb223@go.stockton.edu>
2022-11-01 06:16:33 -0400

    Updated docstrings

--- src/halloweenSourceCode.py
+++ src/halloweenSourceCode.py
@@ -1,18 +1,27 @@
 ############################################
 #          Author: Dale Campbell           #
 #          DSSA 5102-001                   # 
-#          Halloween Assignment            #
-#          Week 8                          # 
+#          Halloween Assignment            # 
 ############################################
 
 # Let's import libaries we'll need
+from cmath import nan
 import pandas as pd
 import numpy as np
+from datetime import datetime
 #import matplotlib as plt
-
 
 def printData(dfa, dfb, dfc):
     # Print an identifier to the file -- data -- new line (to seperate the data for easier reading)
+    """To initally view the data from three provided dataframes the function accepts the dataframes, prints which
+    file it is, the head of the data, then a new line, and repeats for the other two dataframes.
+
+    Args:
+        dfa (Pandas DataFrame): Data from halloween2019a.csv
+        dfb (Pandas DataFrame): Data from halloween2019b.csv
+        dfc (Pandas DataFrame): Data from halloween2019c.csv
+    """
+    
     print('2019a \n')
     print(dfa.head().to_string()) 
     print('\n')
@@ -26,26 +35,49 @@ def printData(dfa, dfb, dfc):
     print('\n')
     
 def splitNamesColumn(dfc):
+    """In the dataframe provived there is one column labeled "Name" -- this function grabs the first and last (if available) names,
+    creates a new dataframe with two columns "firstName" and "lastName" and returns that dataframe.
+
+    Args:
+        dfc (Pandas DataFrame): Data from halloween2019c.csv
+
+    Returns:
+        newdf (Pandas DataFrame): Dataframe with two columns for firstName and lastName (as well as other columns)
+    """
+    
     first = []
     last = []
+    
+    # There are a few cases we'll run into with splitting up names into firstName / lastName
     for name in dfc["Name"]:
+        # Initally we'll split the names
         nameSplit = name.split()
+        
+        # If there's only one name:
+        #   1. The provided name will be the first name
+        #   2. The last name will be null
         if len(nameSplit) == 1:
             first.append(name)
             last.append("")
+        # If it's a perfect split with two names -- firstName=first split and lastName=last split
         elif len(nameSplit) == 2:
             first.append(nameSplit[0])
-            last.append(nameSplit[-1])
+            last.append(nameSplit[-1]) 
+        # If there's three provided names we need to analyze further
         elif len(nameSplit) == 3:
+            # If the final name provided is Jr., Sr., or III -- disregard that part (Jr., Sr., or III)
             if (nameSplit[2] == "Jr.") or (nameSplit[2] == "Sr.") or (nameSplit[2] == "III"):
                 first.append(nameSplit[0])
                 last.append(nameSplit[1])
+            # Otherwise we'll assume it's a middle name and firstName=first split then lastName=last split
             else:
                 first.append(nameSplit[0])
                 last.append(nameSplit[-1])
+        # If there are 4 provided names, firstName=first split and lastName=third split
         elif len(nameSplit) == 4:
             first.append(nameSplit[0])
             last.append(nameSplit[2])
+        # If none of the above cases split the names -- firstName=first split and lastName=last split
         else:
             first.append(nameSplit[0])
             last.append(nameSplit[-1])
@@ -62,38 +94,144 @@ def splitNamesColumn(dfc):
     return newdf
 
 def correctColNames(df):
+    """This function accepts a dataframe then renames the columns in case they are not already in this format then returns the dataframe:
+        firstName
+        lastName
+        DOB
+        DOD
+        Sex
+
+    Args:
+        df (Pandas DataFrame): dataframe that could or could not have the correct column names
+
+    Returns:
+        newdf (Pandas DataFrame): dataframe with the proper column names 
+    """
+    
+    # Here we'll init an empty dataframe and explicitly label the columns in our desired format
     newdf = pd.DataFrame()
     newdf["firstName"] = df.iloc[:,0]
     newdf["lastName"] = df.iloc[:,1]
     newdf["DOB"] = df.iloc[:,2]
     newdf["DOD"] = df.iloc[:,3]
     newdf["Sex"] = df.iloc[:,4]
+    
+    # Finally return the dataframe
     return newdf
 
 def formatGender(df):
+    """This function accepts a dataframe, then standardizes variations of m/M/Male or f/F/Female into:
+        'M'
+        'F'
+
+    Args:
+        df (Pandas DataFrame): dataframe which has a column labeled "Sex"
+
+    Returns:
+        df (Pandas DataFrame): dataframe with male = 'M' and female = 'F'
+    """
+    
     gender = []
     for sex in df["Sex"]:
-        if sex == "Male" or sex == "male" or sex == "m" or sex == "M" or sex == "Male " or sex == "m " or sex == "M " or sex == " Male" or sex == " m" or sex == " M":
+        # Let's remove any leading spaces from the entry
+        sex = str(sex).lstrip()
+        # If the entry is anything related to 'male' it will become 'M'
+        if sex == "Male" or sex == "male" or sex == "m" or sex == "M" or sex == "Male " or sex == "m " or sex == "M ":
             gender.append('M')
-        elif sex == "Female" or sex == "female" or sex == "f" or sex == "F" or sex == "Female " or sex == "f " or sex == "F " or sex == " Female" or sex == " f" or sex == " F":
+        # If the entry is anything related to 'female' it will become 'F'
+        elif sex == "Female" or sex == "female" or sex == "f" or sex == "F" or sex == "Female " or sex == "f " or sex == "F ":
             gender.append('F')
+        # Otherwise, it's a blank entry. 
         else:
             gender.append('')
             
+    # Here we re-write to the column "Sex" with our consistently labaled gender format
     df["Sex"] = pd.DataFrame(gender)
+    
+    # Finally return the dataframe
     return df
 
 def updateDates(df):
+    # I've identified at least the following formats:
+    #   NaN, 1930, jan 14 1947, 04-03-1921
+    #   These seem to be a typo
+    #       Feb 41993 (we'll assume this to be Feb 4 1993)
+    #       02/14/000 (we'll assume this to 02/14/1900)
+    
+    #   If only a year is provided we will format it to 01/01/yyyy
+    #   If only month and year are provided we will format it to mm/01/yyyy
+    
     newDOB = []
     newDOD = []
     
     for dateOfBirth in df["DOB"]:
-        newDOB.append("")
-        newDOD.append("")
+        dateOfBirth = str(dateOfBirth)
+        if dateOfBirth == "02/14/000":
+            newDOB.append(datetime.strptime("02/14/1900", '%m/%d/%Y'))
+            continue
+        elif dateOfBirth == " Feb 41993":
+            newDOB.append(datetime.strptime("04/04/1993", '%m/%d/%Y'))
+            continue
+        elif dateOfBirth == "04-03-1921":
+            newDOB.append(datetime.strptime("04/03/1921", '%m/%d/%Y'))
+            continue
+            
+        dateOfBirth = str(dateOfBirth).lstrip()
+        #print(dateOfBirth)
+
+        if pd.isna(str(dateOfBirth)) or dateOfBirth == "nan":
+            newDOB.append("")
+        elif len(str(dateOfBirth)) == 4:
+            dateOfBirth = "01/01/" + dateOfBirth
+            newDOB.append(datetime.strptime(str(dateOfBirth), '%m/%d/%Y'))
+        else:
+            try:
+                newDOB.append(datetime.strptime(str(dateOfBirth), '%m/%d/%Y'))
+            except:
+                try:
+                    dateOfBirth = str(dateOfBirth).capitalize()
+                    strSplit = dateOfBirth.split()
+                    dateOfBirth = strSplit[0] + "/" + strSplit[1] + "/" + strSplit[2]
+                    print(dateOfBirth + " hit here")
+                    newDOB.append(datetime.strptime(dateOfBirth, '%b/%d/%Y'))
+                except:
+                    newDOB.append(datetime.strptime(str(dateOfBirth), '%B/%d/%Y'))
+                    
+    for dateOfDeath in df["DOD"]:
+        if pd.isna(dateOfDeath):
+            newDOD.append("")
+        elif len(dateOfDeath) == 4:
+            dateOfDeath = "01/01/" + dateOfDeath
+            newDOD.append(datetime.strptime(str(dateOfDeath), '%m/%d/%Y'))
+        else:
+            newDOD.append(datetime.strptime(str(dateOfDeath), '%m/%d/%Y'))
     
-    #df["DOB"] = pd.DataFrame(newDOB)
-    #df["DOD"] = pd.DataFrame(newDOD)
+    df["DOB"] = pd.DataFrame(newDOB)
+    df["DOD"] = pd.DataFrame(newDOD)
     return df
+
+def addLifespan(df):
+    """This function takes in a dataframe, adds a column calcuating the death-birth to generate a lifespan. Then returns the new dataframe
+
+    Args:
+        df (Pandas DataFrame): A dataframe with birth year & death year
+
+    Returns:
+        df (Pandas DataFrame): The same dataframe with one extra column for "lifespan"
+    """
+    
+    lifespan = []
+    birth = []
+    death = []
+    
+    for dateOfBirth in df["DOB"]:
+        birth.append(dateOfBirth)
+    for dateOfDeath in df["DOD"]:
+        death.append(dateOfDeath)
+    #lifespan.append(dateOfDeath - dateOfBirth)
+    #df["Lifespan"] = pd.DataFrame(lifespan)
+    return df
+
 
 def main():
     # Let's read in each of the CSV files to their own dataframes
@@ -108,15 +246,13 @@ def main():
     #             halloween2019a.csv            #           halloween2019b.csv           #   halloween2019c.csv 
     #   FirstName,MiddleName,LastName,DOB,DOD   #   FirstName, LastName, DOB, DOD, Sex   #    Name,DOB,DOD,Sex
     
-    # Based off this, let's prepare a format that's consitent which disregards any middle names
-    #                      firstName, lastName, DOB, DOD, Sex
+    # Let's prepare a format that's consitent which disregards any middle names (firstName, lastName, DOB, DOD, Sex)
     
     # Removing MiddleName from dataframe A
     dfa.pop("MiddleName")
 
     # Adding Empty column for "Sex" to dataframe A 
     dfa["Sex"] = ""
-    #print(dfa.head().to_string()) 
 
     # Dataframe B is in the format we desire 
     # Dataframe C needs the names split up
@@ -127,7 +263,7 @@ def main():
     dfb = correctColNames(dfb)
     dfc = correctColNames(dfc)
     
-    # Now that each of our dataframes is in the desired format -- let's merge them into one dataframe
+    # Now that each of our dataframes are in the desired format -- let's merge them into one dataframe
     df = pd.concat([dfa, dfb, dfc], ignore_index=True)
 
     # OPTIONAL FOR FINAL CODE -- If you would ilke to see all of the data listed, uncomment the next line
@@ -137,16 +273,11 @@ def main():
     # For consistency, let's make these either 'M' or 'F'
     df = formatGender(df)
     
-    # Our dates are very inconsistent, I've identified at least the following formats:
-    #   NaN, 1930, jan 14 1947, 04-03-1921
-    #   These seem to be a typo
-    #       Feb 41993 (we'll assume this to be Feb 4 1993)
-    #       02/14/000 (we'll assume this to 02/14/1900)
-    # Let's make everything a standard format of mm/dd/yyyy
-    #   If only a year is provided we will format it to 01/01/yyyy
-    #   If only month and year are provided we will format it to mm/01/yyyy
+    # Our dates are very inconsistent, let's make everything a standard format of mm/dd/yyyy
+    #df = updateDates(df)
     
-    df = updateDates(df)
+    # Now let's create a new column that calculates the individuals lifespan
+    df = addLifespan(df)
     
     print(df.to_string())
 
